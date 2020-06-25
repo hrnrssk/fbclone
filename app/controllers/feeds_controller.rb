76e5c94cef872ab29b42bfe11ccd27e849435d3f
hironori sasaki <hrnr.ssk@gmail.com>
2020-06-26 00:56:45 +0900

    UserとPictureを紐付け

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -27,7 +27,7 @@ class FeedsController < ApplicationController
   # POST /feeds
   # POST /feeds.json
   def create
-    @feed = Feed.new(feed_params)
+    @feed = current_user.feeds.build(feed_params)
     respond_to do |format|
       if @feed.save
         format.html { redirect_to @feed, notice: 'Feed was successfully created.' }
@@ -63,7 +63,8 @@ class FeedsController < ApplicationController
     end
   end
   def confirm
-    @feed = Feed.new(feed_params)
+    @feed = current_user.feeds.build(feed_params)
+    render :new if @feed.invalid?
   end
 
   private
